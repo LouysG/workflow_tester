4c865b3e750a7bdd1af0010ff48f39e073c62056
Louys Gackstetter <louys.gackstetter@gmail.com>
2025-11-11 19:42:22 -0700

    Initial Commit

---
+++ src/my_package/my_module.py
@@ -0,0 +1,21 @@
+import json
+import re
+import pytest
+import os
+import sys
+import datetime
+
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+
+
+
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    return x / y
