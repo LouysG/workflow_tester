f05032ebde7c71b53cae572a3d3af103df0f7248
Louys Gackstetter <louys.gackstetter@gmail.com>
2025-11-11 20:51:27 -0700

    Test #10 (testing pre-commit hooks)

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -5,13 +5,16 @@ def test_add():
     assert add(1, 2) == 3
     assert add(3, 4) == 7
 
+
 def test_subtract():
     assert subtract(1, 2) == -1
     assert subtract(3, 4) == -1
 
+
 def test_multiply():
     assert multiply(1, 2) == 2
     assert multiply(3, 4) == 12
+
 
 def test_divide():
     assert divide(1, 2) == 0.5
